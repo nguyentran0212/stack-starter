9cdd550139d0b0892bbb53b6ea0a96f5ae5c86b4
Nguyen Tran <nguyen.tran@adelaide.edu.au>
2024-11-22 13:47:18 +1030

    Finished scaffolding of the CLI arg parse

--- stack_starter/cli.py
+++ stack_starter/cli.py
@@ -1,5 +1,47 @@
+import argparse
+
+def parse_sys_args():
+    # Setup top level parser
+    parser = argparse.ArgumentParser(description="A utility for provision machines and configure software stacks based on predefined recipes")
+    parser.add_argument("infra", help="Name of the infrastructure to provision or configure", default="localhost")
+    parser.add_argument("-d", "--directory", help="Specify the working directory", default="/tmp/stack_starter/")
+    subparsers = parser.add_subparsers(title="CMD", description="Sub-commands")
+
+    # Setup sub-parser for provision sub-command
+    parser_provision = subparsers.add_parser("provision", description="Provision machines and networks from bare metal or cloud providers", epilog="Return an Ansible hostfile of the provisioned machines")
+    parser_provision.add_argument("provider", help="Infrastructure provider for the machines to provision", default="localhost")
+    parser_provision.add_argument("recipe", help="Recipe for provisioning")
+    parser_provision.set_defaults(cmd="provision")
+
+    # Setup sub-parser for configure sub-command
+    parser_configure = subparsers.add_parser("configure", description="Configure software stack on a specified infrastructure")
+    parser_configure.add_argument("recipe", help="Recipe for configure the infrastructure")
+    parser_configure.set_defaults(cmd="configure")
+    
+    # Parse and return arguments in Name space object
+    # Example: Namespace(infra='home', directory='/tmp/stack_starter/', recipe='mac_os_host', cmd='configure')
+    return parser.parse_args()
+
+def provision(provider : str, recipe : str):
+    print(f"Provider: {provider}")
+    print(f"Recipe: {recipe}")
+    pass
+
+def configure(recipe : str): 
+    print(f"Recipe: {recipe}")
+    pass
+
 def main():
-    print("Welcome to Stack Starter!")
+    args = parse_sys_args()
+
+    if args.cmd == "provision":
+        provision(args.provider, args.recipe)
+    elif args.cmd == "configure":
+        configure(args.recipe) 
+
+    
+
+    # print("Welcome to Stack Starter!")
     # Add your CLI logic here
 
 if __name__ == "__main__":
