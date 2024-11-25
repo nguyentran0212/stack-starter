787623f590069061e414e5b1a02bb9700a0eddc3
Nguyen Tran <nguyen.tran@adelaide.edu.au>
2024-11-25 12:13:51 +1030

    Added spec of configuration file

--- stack_starter/cli.py
+++ stack_starter/cli.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 import subprocess
+from typing import Tuple
+from typing_extensions import Dict
 
 def parse_sys_args():
     # Setup top level parser
@@ -56,17 +58,34 @@ def configure(infra : str, recipe : str, working_dir: str, recipe_path: str):
     
     subprocess.run(ansible_command, check=True)
 
+def prepare_working_dir(working_dir : str): 
+    pass
+
+def load_recipes(recipe_dir : str) -> Tuple[Dict[str, str], Dict[str, str]]:
+    provision_recipes = {}
+    configure_recipes = {}
+    return provision_recipes, configure_recipes
+
 def main():
+    # Parse system args
     args = parse_sys_args()
+    # Setup paths, prepare working directory, and load recipes
     script_dir = os.path.dirname(os.path.abspath(__file__))    
     working_dir = os.path.abspath(args.directory) if os.path.isabs(args.directory) else os.path.join(script_dir, args.directory)
     recipe_dir = os.path.abspath(args.recipe_path) if os.path.isabs(args.recipe_path) else os.path.join(script_dir, args.recipe_path)
     configure_recipe_dir = os.path.join(recipe_dir, "configure")
     provision_recipe_dir = os.path.join(recipe_dir, "provision")
 
+    # Prepare working directory
+    prepare_working_dir(working_dir)
+    # Detect and load all the recipes
+    provision_recipes, configure_recipes = load_recipes(recipe_dir)
+
     if args.cmd == "provision":
+        # Throw if the required provision_recipe is not available
         provision(args.infra, args.provider, args.recipe)
     elif args.cmd == "configure":
+        # Throw if the required configure recipe is not available 
         configure(args.infra, args.recipe, working_dir, configure_recipe_dir) 
 
     
